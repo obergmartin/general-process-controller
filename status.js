cbf2588f246995d379e4d256b2595ad554975910
Martin <obergma@gmail.com>
2017-05-16 17:27:29 -0700

    added help page

--- status.js
+++ status.js
@@ -1,16 +1,14 @@
 var temptext = document.getElementById('temptext');
 var setpointtext = document.getElementById('setpointtext');
+var relaytext = document.getElementById('relaytext');
 var timetext = document.getElementById('timetext');
-
 
 timetext.innerText="Last reading at: " + recent.Time;
 temptext.innerText="Sensor Temp: " + recent.temp1;
 setpointtext.innerText="Setpoint: " + recent.warmPt;
-relaytext.innerText="The relay is " + ["Off", "On"][recent.isOn];
+relaytext.innerText="The relay is " + ["Off", "On"][+recent.isOn];
 temp2text.innerText="Tap Temp: " + recent.temp2;
 //floortext.innerText="The floor is currently " + ["nice and dry.", "FLOODED!"][0];
-
-
 
 var httpRequest = new XMLHttpRequest();
 httpRequest.onreadystatechange = function() {
@@ -21,7 +19,6 @@ httpRequest.onreadystatechange = function() {
             //init = data.init;
             //console.log(init);
             //show_page();
-            
             //return data;
         }
     }
