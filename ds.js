f61bd72e8e41103b378880faa87aaca5c3b334e2
Martin <obergma@gmail.com>
2017-05-20 09:11:04 -0700

    add separate getTempC and getTempF functions

--- ds.js
+++ ds.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var ChildProcess = require('child_process');
 
 function DS(addr){
 	// for future use:
@@ -19,8 +20,6 @@ DS.prototype.getTemp = function(){
 	//2e 00 4b 46 7f ff 02 10 05 : crc=05 YES
     //2e 00 4b 46 7f ff 02 10 05 t=2875
 
-    var ChildProcess = require('child_process');
-
     var result = ChildProcess.execSync(this.cmd).toString().trim();
     
     var i = result.search('t=');
@@ -29,6 +28,23 @@ DS.prototype.getTemp = function(){
     if (this.unit === 'F') {
         t = (t * 1.8) + 32;
     }
+    t = parseFloat(t.toFixed(1));
+    return t;
+};
+
+DS.prototype.getTempC = function(){
+    var result = ChildProcess.execSync(this.cmd).toString().trim();
+
+    var i = result.search('t=');
+    var t = result.substring(i+2) / 1000;
+    t = parseFloat(t.toFixed(1));
+
+    return t;
+};
+
+DS.prototype.getTempF = function(){
+	var t = this.getTempC();
+    t = (t * 1.8) + 32;
     t = parseFloat(t.toFixed(1));
     return t;
 };
