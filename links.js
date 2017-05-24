c06f3acc16a27180cb92fbe8952bb524daef0d11
Martin <obergma@gmail.com>
2017-05-24 16:45:21 -0700

    DS devices.html and links.js navigation file

---
+++ links.js
@@ -0,0 +1,5 @@
+document.write("<a href='setup.html'>Setup Page</a> ");
+document.write("<a href='devices.html'>Devices Page</a> ");
+document.write("<a href='status.html'>Status Page</a> ");
+document.write("<a href='loghistory.html'>Data Logs</a> ");
+document.write("<a href='help.html'>Help</a>");
