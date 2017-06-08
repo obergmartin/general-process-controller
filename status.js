248781e996fd46624653b2ed6c4f187d04ab0eb7
Martin <obergma@gmail.com>
2017-06-08 16:36:17 -0700

    status.html now configured by status.json

--- status.js
+++ status.js
@@ -1,36 +1,43 @@
-var temptext = document.getElementById('temptext');
-var setpointtext = document.getElementById('setpointtext');
-var relaytext = document.getElementById('relaytext');
-var timetext = document.getElementById('timetext');
+//var temptext = document.getElementById('temptext');
+//var setpointtext = document.getElementById('setpointtext');
+//var relaytext = document.getElementById('relaytext');
+//var timetext = document.getElementById('timetext');
 
-timetext.innerText="Last reading at: " + recent.Time;
-temptext.innerText="Sensor Temp: " + recent.temp1;
-setpointtext.innerText="Setpoint: " + recent.warmPt;
-relaytext.innerText="The relay is: " + ["Off", "On"][+recent["isOn]"]];
-temp2text.innerText="Tap Temp: " + recent.temp2;
+//timetext.innerText="Last reading at: " + recent.Time;
+//temptext.innerText="Sensor Temp: " + recent.temp1;
+//setpointtext.innerText="Setpoint: " + recent.warmPt;
+//relaytext.innerText="The relay is: " + ["Off", "On"][+recent["isOn]"]];
+//temp2text.innerText="Tap Temp: " + recent.temp2;
 //floortext.innerText="The floor is currently " + ["nice and dry.", "FLOODED!"][0];
-
 var httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = function() {
-    if (httpRequest.readyState === 4) {
-        if (httpRequest.status === 200) {
-            //console.log(httpRequest.responseText);
-            //document.data = JSON.parse(httpRequest.responseText.slice(7, httpRequest.responseText.length));
-            //init = data.init;
-            //console.log(init);
-            //show_page();
-            //return data;
-        }
-    }
-};
+
+httpRequest.open('GET', 'status.json', false);
+httpRequest.send();
+var stat = JSON.parse(httpRequest.responseText).stat;
+
+var area = document.getElementById('main');
+
+for (var v in stat){
+  cur = document.createElement("p");
+  console.log(stat[v]);
+  cur.innerText = stat[v].Label;
+  if (stat[v].Value === '') {
+    cur.innerText += recent[v];
+  } else {
+    cur.innerText += stat[v].Value[+recent[v]];
+  }
+  area.appendChild(cur);
+  
+  
+}
+
+
 
 var getfile = recent.date+'.csv';
-//console.log(getfile);
 httpRequest.open('GET', getfile, false);
 httpRequest.send();
-//console.log('response');
-//console.log(httpRequest.responseText);
 var data = JSON.parse(httpRequest.responseText.slice(7, httpRequest.responseText.length)); 
+
 //console.log(data);
 
 plot_it();
