d031f2e395adb799e86fe099b18343911a84636a
Martin <obergma@gmail.com>
2017-06-08 16:48:39 -0700

    add edit links and clean up

--- status.js
+++ status.js
@@ -1,14 +1,4 @@
-//var temptext = document.getElementById('temptext');
-//var setpointtext = document.getElementById('setpointtext');
-//var relaytext = document.getElementById('relaytext');
-//var timetext = document.getElementById('timetext');
 
-//timetext.innerText="Last reading at: " + recent.Time;
-//temptext.innerText="Sensor Temp: " + recent.temp1;
-//setpointtext.innerText="Setpoint: " + recent.warmPt;
-//relaytext.innerText="The relay is: " + ["Off", "On"][+recent["isOn]"]];
-//temp2text.innerText="Tap Temp: " + recent.temp2;
-//floortext.innerText="The floor is currently " + ["nice and dry.", "FLOODED!"][0];
 var httpRequest = new XMLHttpRequest();
 
 httpRequest.open('GET', 'status.json', false);
@@ -27,10 +17,7 @@ for (var v in stat){
     cur.innerText += stat[v].Value[+recent[v]];
   }
   area.appendChild(cur);
-  
-  
 }
-
 
 
 var getfile = recent.date+'.csv';
@@ -38,11 +25,4 @@ httpRequest.open('GET', getfile, false);
 httpRequest.send();
 var data = JSON.parse(httpRequest.responseText.slice(7, httpRequest.responseText.length)); 
 
-//console.log(data);
-
 plot_it();
-    
-    
-    
-    
-    
