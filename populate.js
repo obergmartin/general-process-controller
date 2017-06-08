8356f8f507ad53080f96be26c42afb75bd5aed9f
Martin <obergma@gmail.com>
2017-06-08 11:18:26 -0700

    formatting

--- populate.js
+++ populate.js
@@ -70,7 +70,7 @@ function update_page() {
 
   // Save Devices
   dat.Devices = [];
-  for (var i=1; i<tables.Devices.rows.length; i++) {
+  for (var i = 1; i < tables.Devices.rows.length; i++) {
     var r = {};
     r.Name = tables.Devices.rows[i].cells[0].children[0].value;
     r.Type = tables.Devices.rows[i].cells[1].children[0].value;
@@ -83,7 +83,7 @@ function update_page() {
   
   // Save Input Values
   dat.InputVals = [];
-  for (i=1; i<tables.InputVals.rows.length; i++) {
+  for (i = 1; i < tables.InputVals.rows.length; i++) {
     var r = {};
     r.Name =    tables.InputVals.rows[i].cells[0].children[0].value;
     isChecked = tables.InputVals.rows[i].cells[1].children[0].checked;
@@ -99,7 +99,7 @@ function update_page() {
   
   // Save Evals
   dat.Evals = [];
-  for (i=1; i< tables.Evals.rows.length; i++) {
+  for (i = 1; i < tables.Evals.rows.length; i++) {
     var r = {};
     r.Name =     tables.Evals.rows[i].cells[0].children[0].value;
     isChecked =  tables.Evals.rows[i].cells[1].children[0].checked;
@@ -126,7 +126,7 @@ function update_page() {
   
   // Save Actions
   dat.Actions = [];
-  for (i=1; i< tables.Actions.rows.length; i++) {
+  for (i = 1; i < tables.Actions.rows.length; i++) {
     var r = {};
     r.Trigger = tables.Actions.rows[i].cells[0].children[0].value;
     r.Object = tables.Actions.rows[i].cells[1].children[0].value;
@@ -140,19 +140,19 @@ function update_page() {
   
   xhr = new XMLHttpRequest();
   
-    var url = "/setup.html";
-    xhr.open("POST", url);//, true);
-    xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-    xhr.onreadystatechange = function () { 
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            //var json = JSON.parse(xhr.responseText);
-            console.log(xhr.responseText);
-            alert(xhr.responseText);
-        }
-    };
-    var dataSend = JSON.stringify({'init': dat}, null, ' ');
-    console.log(dataSend);
-    xhr.send(dataSend);
+  var url = "/setup.html";
+  xhr.open("POST", url);//, true);
+  xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+  xhr.onreadystatechange = function () { 
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      //var json = JSON.parse(xhr.responseText);
+      console.log(xhr.responseText);
+      alert(xhr.responseText);
+    }
+  };
+  var dataSend = JSON.stringify({'init': dat}, null, ' ');
+  console.log(dataSend);
+  xhr.send(dataSend);
   window.location.href = "setup.html";
 }
 
@@ -202,7 +202,7 @@ function make_row(cellInfo) {
 //
   var r = document.createElement('tr');
   
-  for (var i=0; i<cellInfo.length; i++) {
+  for (var i = 0; i < cellInfo.length; i++) {
     var cur = cellInfo[i];
     var new_cell = document.createElement('td');
     var content = document.createElement('p');
@@ -220,7 +220,7 @@ function make_row(cellInfo) {
     }
     else if (cur.type === "selectMenu"){
       content = document.createElement("select");
-      for (var j=0; j<cur.val[0].length; j++) {
+      for (var j = 0; j < cur.val[0].length; j++) {
         content.options.add( new Option(cur.val[0][j], cur.val[0][j]) );
       }
       content.value = cur.val[1];
@@ -243,183 +243,180 @@ function make_row(cellInfo) {
 
 
 function show_page(){
-//
-// Device Setup
-//
-var avail_devs = [];
-var dev_area = document.getElementById("input_dev_area");
-dev_area.align = 'left';
+  //
+  // Device Setup
+  //
+  var avail_devs = [];
+  var dev_area = document.getElementById("input_dev_area");
+  dev_area.align = 'left';
 
-//make table
-tables.Devices = document.createElement('table');
-var sortableDevices = Sortable.create(tables.Devices, {
-  filter: '.js-remove',
-  onFilter: function (evt) {
-    var el = editableList.closest(evt.item); // get dragged item
-    el && el.parentNode.removeChild(el);
+  //make table
+  tables.Devices = document.createElement('table');
+  var sortableDevices = Sortable.create(tables.Devices, {
+    filter: '.js-remove',
+    onFilter: function (evt) {
+      var el = editableList.closest(evt.item); // get dragged item
+      el && el.parentNode.removeChild(el);
+    }
+  });
+  var header = tables.Devices.createTHead();
+  header.appendChild( make_row(['Name', 'Device', 'Address']) );
+
+  // Add rows from DEVICES section  
+  for (var i = 0; i < init.Devices.length; i++) {
+    var new_row = make_row( [
+      {'type': 'inputText', 'val': init.Devices[i].Name},
+      {'type': 'selectMenu', 'val': [["DS","gpio", "relayexp","MyClock"], init.Devices[i].Type]},
+      {'type': 'inputText', 'val': init.Devices[i].addr} ]);
+    tables.Devices.appendChild(new_row);
+    avail_devs.push(init.Devices[i].Name);
   }
-});
-var header = tables.Devices.createTHead();
-header.appendChild( make_row(['Name', 'Device', 'Address']) );
+  dev_area.appendChild(tables.Devices);
 
-// Add rows from DEVICES section  
-for (var i=0; i<init.Devices.length; i++) {
-  var new_row = make_row( [
-    {'type': 'inputText', 'val': init.Devices[i].Name},
-    {'type': 'selectMenu', 'val': [["DS","gpio", "relayexp","MyClock"], init.Devices[i].Type]},
-    {'type': 'inputText', 'val': init.Devices[i].addr} ]);
-  tables.Devices.appendChild(new_row);
-  avail_devs.push(init.Devices[i].Name);
-}
-dev_area.appendChild(tables.Devices);
-
-// Button to add new rows
-var dev_button = document.createElement("input");
-dev_button.type = 'button';
-dev_button.value = "Add Row";
-dev_button.onclick = function(){ 
-  var new_row = make_row( [
+  // Button to add new rows
+  var dev_button = document.createElement("input");
+  dev_button.type = 'button';
+  dev_button.value = 'Add Row';
+  dev_button.onclick = function(){ 
+    var new_row = make_row( [
     {'type': 'inputText', 'val': ''},
     {'type': 'selectMenu', 'val': [["DS","gpio", "relayexp","MyClock"], "Select"]},
     {'type': 'inputText', 'val': ''} ]);
-  tables.Devices.appendChild(new_row);
-};
+    tables.Devices.appendChild(new_row);
+  };
 
-dev_area.appendChild(dev_button);
+  dev_area.appendChild(dev_button);
 
-
-//
-// Input Variables
-//
-var var_area = document.getElementById("input_var_area");
-var_area.align = 'left';
+  //
+  // Input Variables
+  //
+  var var_area = document.getElementById("input_var_area");
+  var_area.align = 'left';
       
-//make table
-tables.InputVals = document.createElement('table');
-var sortableInputVals = Sortable.create(tables.InputVals);
-header = tables.InputVals.createTHead();
-header.appendChild( make_row(['Name', 'Log', 'Device', 'Function']) );
+  //make table
+  tables.InputVals = document.createElement('table');
+  var sortableInputVals = Sortable.create(tables.InputVals);
+  header = tables.InputVals.createTHead();
+  header.appendChild( make_row(['Name', 'Log', 'Device', 'Function']) );
   
-for (i=0; i<init.InputVals.length; i++) {
-  var isChecked = (init.DataLog.indexOf(init.InputVals[i].Name) != -1);
-  var avail_fns = deviceFunctionsInput[getDeviceType(init.InputVals[i].Device)];
+  for (i = 0; i < init.InputVals.length; i++) {
+    var isChecked = (init.DataLog.indexOf(init.InputVals[i].Name) != -1);
+    var avail_fns = deviceFunctionsInput[getDeviceType(init.InputVals[i].Device)];
+    var new_row = make_row( [
+      {'type': 'inputText', 'val': init.InputVals[i].Name},
+      {'type': 'checkBox', 'val': isChecked},
+      {'type': 'selectMenu', 'val': [avail_devs, init.InputVals[i].Device]},
+      {'type': 'selectMenu', 'val': [avail_fns, init.InputVals[i].fn]}
+    ]);
+    new_row.children[2].children[0].onchange = changeInputSelect;
+    tables.InputVals.appendChild(new_row);
+
+  }
+  var_area.appendChild(tables.InputVals);
+
+  var var_button = document.createElement("input");
+  var_button.type = 'button';
+  var_button.value = 'Add Row';
+  var_button.onclick = function() {
+    var new_row = make_row( [
+      {'type': 'inputText', 'val': ''},
+      {'type': 'checkBox', 'val': 0},
+      {'type': 'selectMenu', 'val': [avail_devs, "Select"]},
+      {'type': 'selectMenu', 'val': ['','']}
+    ]);
+    new_row.children[2].children[0].onchange = changeInputSelect;
+    tables.InputVals.appendChild(new_row);};
+
+  var_area.appendChild(var_button);
+
+  //
+  // Evaluated Variables
+  //
+  var eval_area = document.getElementById("eval_var_area");
+  var_area.align = 'left';
+
+  //make table
+  tables.Evals = document.createElement('table');
+  var sortableEvals = Sortable.create(tables.Evals);
+  header = tables.Evals.createTHead();
+  header.appendChild( make_row(['Name', 'Log', 'Eval']) );
+  
+  for (i = 0; i < init.Evals.length; i++) {
+    var isChecked = (init.DataLog.indexOf(init.Evals[i].Name) != -1);
+    var new_row = make_row( [
+      {'type': 'inputText', 'val': init.Evals[i].Name},
+      {'type': 'checkBox', 'val': isChecked},
+      {'type': 'inputText', 'val': init.Evals[i].Eval} 
+    ]);
+    tables.Evals.appendChild(new_row);  
+  }
+
+  eval_area.appendChild(tables.Evals);
+
+  var eval_button = document.createElement('input');
+  eval_button.type = 'button';
+  eval_button.value = 'Add Row';
+  eval_button.onclick = function(){ 
   var new_row = make_row( [
-    {'type': 'inputText', 'val': init.InputVals[i].Name},
-    {'type': 'checkBox', 'val': isChecked},
-    {'type': 'selectMenu', 'val': [avail_devs, init.InputVals[i].Device]},
-    {'type': 'selectMenu', 'val': [avail_fns, init.InputVals[i].fn]}
-  ]);
-  new_row.children[2].children[0].onchange = changeInputSelect;
-  tables.InputVals.appendChild(new_row);
-  
-}
-var_area.appendChild(tables.InputVals);
-
-var var_button = document.createElement("input");
-var_button.type = 'button';
-var_button.value = "Add Row";
-var_button.onclick = function() {
-  var new_row = make_row( [
-    {'type': 'inputText', 'val': ''},
-    {'type': 'checkBox', 'val': 0},
-    {'type': 'selectMenu', 'val': [avail_devs, "Select"]},
-    {'type': 'selectMenu', 'val': ['','']}
-  ]);
-  new_row.children[2].children[0].onchange = changeInputSelect;
-  tables.InputVals.appendChild(new_row);};
-
-var_area.appendChild(var_button);
-
-
-//
-// Evaluated Variables
-//
-var eval_area = document.getElementById("eval_var_area");
-var_area.align = 'left';
-
-//make table
-tables.Evals = document.createElement('table');
-var sortableEvals = Sortable.create(tables.Evals);
-header = tables.Evals.createTHead();
-header.appendChild( make_row(['Name', 'Log', 'Eval']) );
-
-  
-for (i=0; i<init.Evals.length; i++) {
-    
-  var isChecked = (init.DataLog.indexOf(init.Evals[i].Name) != -1);
-  
-  var new_row = make_row( [
-    {'type': 'inputText', 'val': init.Evals[i].Name},
-    {'type': 'checkBox', 'val': isChecked},
-    {'type': 'inputText', 'val': init.Evals[i].Eval} ]);
-  tables.Evals.appendChild(new_row);  
-}
-
-eval_area.appendChild(tables.Evals);
-
-var eval_button = document.createElement("input");
-eval_button.type = 'button';
-eval_button.value = "Add Row";
-eval_button.onclick = function(){ 
-var new_row = make_row( [
     {'type': 'inputText', 'val': ''},
     {'type': 'checkBox', 'val': 0},
     {'type': 'inputText', 'val': ''} 
-]);
-tables.Evals.appendChild(new_row);};
-
-eval_area.appendChild(eval_button);
-
-
-//
-// Action Setup
-//
-
-//get parent
-var act_area = document.getElementById("input_act_area");
-act_area.align = 'left';
-
-//make table
-tables.Actions = document.createElement('table');
-var sortableActions = Sortable.create(tables.Actions);
-header = tables.Actions.createTHead();
-header.appendChild( make_row(['Name', 'Device', 'Action']) );
-
-for (i=0; i<init.Actions.length; i++) {
-  var cur_dev = getDeviceType(init.Actions[i].Object);
-  var new_row = make_row( [
-    {'type': 'inputText', 'val': init.Actions[i].Trigger},
-    {'type': 'selectMenu', 'val': [avail_devs, init.Actions[i].Object]},
-    {'type': 'selectMenu', 'val': [deviceFunctionsOutput[cur_dev], init.Actions[i].fn]}
   ]);
-  new_row.children[1].children[0].onchange = changeActionSelect;
-  tables.Actions.appendChild(new_row);
+  tables.Evals.appendChild(new_row);};
+
+  eval_area.appendChild(eval_button);
+
+  //
+  // Action Setup
+  //
+
+  //get parent
+  var act_area = document.getElementById('input_act_area');
+  act_area.align = 'left';
+
+  //make table
+  tables.Actions = document.createElement('table');
+  var sortableActions = Sortable.create(tables.Actions);
+  header = tables.Actions.createTHead();
+  header.appendChild( make_row(['Name', 'Device', 'Action']) );
+
+  for (i = 0; i < init.Actions.length; i++) {
+    var cur_dev = getDeviceType(init.Actions[i].Object);
+    var new_row = make_row( [
+      {'type': 'inputText', 'val': init.Actions[i].Trigger},
+      {'type': 'selectMenu', 'val': [avail_devs, init.Actions[i].Object]},
+      {'type': 'selectMenu', 'val': [deviceFunctionsOutput[cur_dev], init.Actions[i].fn]}
+    ]);
+    new_row.children[1].children[0].onchange = changeActionSelect;
+    tables.Actions.appendChild(new_row);
+  }
+  act_area.appendChild(tables.Actions);
+
+  var action_button = document.createElement('input');
+  action_button.type = 'button';
+  action_button.value = 'Add Row';
+  action_button.onclick = function(){ 
+    var new_row = make_row( [
+      {'type': 'inputText', 'val': ''},
+      {'type': 'selectMenu', 'val': [avail_devs, 'Select']},
+      {'type': 'selectMenu', 'val': ['', '']}
+    ]);
+    new_row.children[1].children[0].onchange = changeActionSelect;
+    tables.Actions.appendChild(new_row);
+  };
+
+  act_area.appendChild(action_button);
+
+  var dat = {};
+
+  var tail_area = document.getElementById('tail_area');
+
+  var update_button = document.getElementById('update_button');
+  update_button.value = 'Update!';
+  update_button.onclick = function(){ update_page(); };
+
+  tail_area.appendChild(update_button);
 }
-act_area.appendChild(tables.Actions);
-
-var action_button = document.createElement("input");
-action_button.type = 'button';
-action_button.value = "Add Row";
-action_button.onclick = function(){ 
-  var new_row = make_row( [
-    {'type': 'inputText', 'val': ''},
-    {'type': 'selectMenu', 'val': [avail_devs, "Select"]},
-    {'type': 'selectMenu', 'val': ['', '']}
-  ]);
-  new_row.children[1].children[0].onchange = changeActionSelect;
-  tables.Actions.appendChild(new_row);
-};
-
-act_area.appendChild(action_button);
-
-var dat = {};
 
 
-var tail_area = document.getElementById("tail_area");
 
-var update_button = document.getElementById("update_button");
-update_button.value = "Update!";
-update_button.onclick = function(){ update_page(); };
-
-tail_area.appendChild(update_button);
-}
