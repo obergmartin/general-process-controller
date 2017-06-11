fe00ccca0aa234b835d5726d2904ba8c31de38c7
Martin <obergma@gmail.com>
2017-06-10 20:39:19 -0700

    feat: better interval timing

--- control.js
+++ control.js
@@ -9,5 +9,56 @@ var mainLoop = function() {
 };
 
 console.log("Starting Agent...");
-mainLoop();
-setInterval(mainLoop, 60000);
+//mainLoop();
+
+//setInterval(mainLoop, 60000);
+
+/**
+ * Self-adjusting interval to account for drifting
+ * 
+ * @param {function} workFunc  Callback containing the work to be done
+ *                             for each interval
+ * @param {int}      interval  Interval speed (in milliseconds) - This 
+ * @param {function} errorFunc (Optional) Callback to run if the drift
+ *                             exceeds interval
+ */
+function AdjustingInterval(workFunc, interval, errorFunc) {
+    var that = this;
+    var expected, timeout;
+    this.interval = interval;
+
+    this.start = function() {
+        expected = Date.now() + this.interval;
+        timeout = setTimeout(step, this.interval);
+    }
+
+    this.stop = function() {
+        clearTimeout(timeout);
+    }
+
+    function step() {
+        var drift = Date.now() - expected;
+        if (drift > that.interval) {
+            // You could have some default stuff here too...
+            if (errorFunc) errorFunc();
+        }
+        workFunc();
+        expected += that.interval;
+        timeout = setTimeout(step, Math.max(0, that.interval-drift));
+    }
+}
+
+// Define the work to be done
+//var doWork = function() {
+//    console.log(++justSomeNumber);
+//};
+
+// Define what to do if something goes wrong
+var doError = function() {
+    console.warn('The drift exceeded the interval.');
+};
+
+// (The third argument is optional)
+var ticker = new AdjustingInterval(mainLoop, 60000, doError);
+ticker.start();
+
