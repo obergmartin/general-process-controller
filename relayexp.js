9702e4e5ffb478ccb54e67c9fd7db17bc50ba9d9
obergmartin <obergma@gmail.com>
2017-06-11 19:54:48 -0700

    comments

--- relayexp.js
+++ relayexp.js
@@ -1,18 +1,15 @@
-//rewrite to use:
 //https://docs.onion.io/omega2-docs/relay-expansion-node-module.html
 'use strict';
 
-var ChildProcess = require('child_process');
 var relay = require('/usr/bin/node-relay-exp');
 
 function RelayExp(addr, channel){
-//function RelayExp(addr){
     this.addr = addr;
     this.channel = channel;
+    relay.init(addr);
     this.lastOn = 0;
     this.setRelayOff();
     this.lastOff = 0;//+ Date();
-    relay.init(addr);
 }
 
 RelayExp.prototype.getStatus = function(){
@@ -36,28 +33,27 @@ RelayExp.prototype.getTimeOff = function(){
 };
 
 RelayExp.prototype.getMinutesOn = function(){
+    // Returns 0 if OFF
+    // Returns Minutes on if ON
     return this.isOn() * (+ new Date() - this.lastOn) / 60000;
 };
 
 RelayExp.prototype.getMinutesOff = function(){
+    // Returns 0 if ON
+    // Returns Minutes off if OFF
     return this.isOff() * (+ new Date() - this.lastOff) / 60000;
 };
 
 RelayExp.prototype.setRelayOn = function(){
-    var cmd = "relay-exp " + this.addr + " on";
-    //ChildProcess.execSync(cmd);
     relay.setChannel(this.addr, this.channel, 1);
     this.status = 1;
     this.lastOn = + new Date();
 };
 
 RelayExp.prototype.setRelayOff = function(){
-    var cmd = "relay-exp " + this.addr + " off";
-    //ChildProcess.execSync(cmd);
     relay.setChannel(this.addr, this.channel, 0);
     this.status = 0;
     this.lastOff = + new Date();
 };
 
- 
 module.exports = RelayExp;
